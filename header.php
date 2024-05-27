4687289970f6fe49befe682149446fda1af35329
fabiogabrielli <fabiomgabrielli@gmail.com>
2024-05-26 23:03:09 -0300

    Esqueleto inicial

--- header.php
+++ header.php
@@ -4,5 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forever Two Wheels</title>
+
+    <!-- CSS PROPRIO -->
+    <link rel="stylesheet" href="cssIndex.css" media="all">
+
 </head>
 <body>
--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php include "header.php" ?>
     
+<header class="stage">
+    <nav class="nav container">
+        <div><img class="logo" src="imgs/logo_ftw_social_black.svg" alt="Logo"></div>
+        <div class="menuLinks">MENU</div>
+        <div class="btnLog">BOTÃO 01 BOTÃO 02</div>
+    </nav>
+</header>
 
-TESTE
+<main class="stage">
+    <div class="hero container">
+
+    </div>
+</main>
 
 
 <?php include "footer.php" ?>
