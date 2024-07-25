a4738d05ac4e6269a0016878d7e6bc384669c750
fabiogabrielli <fabiomgabrielli@gmail.com>
2024-07-24 21:30:32 -0300

    new sections

--- index.php
+++ index.php
@@ -51,10 +51,8 @@ while ($row = $resultado->fetch_assoc()) {
 
 <!-- BANNER -->
 
-<section class="banner container-fluid">
-    <span class="tag tagPress"><?= $press[3]["categoria_news"]; ?></span>
-            <h3 class="Titulo"><?= $press[3]["titulo_news"]; ?></h3>
-            <h4 class="SubTitulo"><?= $press[3]["titulo_news"]; ?></h4>
+<section class="banner01">
+    <div class="efeitoBanner01">PUBLICIDADE</div>
 </section>
 
 
@@ -88,7 +86,10 @@ while ($row = $resultado->fetch_assoc()) {
 <!-- EVENTOS -->
 
 <section id="eventos" class="container-fluid">
-    <div class="container"><span class="tag tagEventos">EVENTOS</span></div><br>
+    <div class="container">
+        <h5 class="tagEventos">EVENTOS</h5>
+        <h1 class="Titulo titAgenda">Confira a agenda de eventos do mercado segurador</h1>
+    </div>
     <div class="eventos container">
         <div class="evento">01</div>
         <div class="evento">02</div>
@@ -100,5 +101,92 @@ while ($row = $resultado->fetch_assoc()) {
         <div class="evento">08</div>
     </div>
 </section>
+
+<!-- BANNER -->
+
+<section class="banner01">
+    <div class="efeitoBanner01">PUBLICIDADE</div>
+</section>
+
+<!-- DEMAIS NEWS -->
+
+<section id="maisNews" class="container-fluid">
+    <div class="container">
+        <div class="cardNews">
+            <div class="cardNewsImg"></div>
+            <h5 class="tagMaisNews">EVENTOS</h5>
+            <h3>Teste de título</h3>
+            <h4>Aqui vai o subtítulo da matéria, exclusiva ou press release</h4>
+        </div>
+        <div class="cardNews">
+            <div class="cardNewsImg"></div>
+            <h5 class="tagMaisNews">EVENTOS</h5>
+            <h3>Teste de título</h3>
+            <h4>Aqui vai o subtítulo da matéria, exclusiva ou press release</h4>
+        </div>
+        <div class="cardNews">
+            <div class="cardNewsImg"></div>
+            <h5 class="tagMaisNews">EVENTOS</h5>
+            <h3>Teste de título</h3>
+            <h4>Aqui vai o subtítulo da matéria, exclusiva ou press release</h4>
+        </div>
+        <div class="cardNews">
+            <div class="cardNewsImg"></div>
+            <h5 class="tagMaisNews">EVENTOS</h5>
+            <h3>Teste de título</h3>
+            <h4>Aqui vai o subtítulo da matéria, exclusiva ou press release</h4>
+        </div>
+        <div class="cardNews">
+            <div class="cardNewsImg"></div>
+            <h5 class="tagMaisNews">EVENTOS</h5>
+            <h3>Teste de título</h3>
+            <h4>Aqui vai o subtítulo da matéria, exclusiva ou press release</h4>
+        </div>
+        <div class="cardNews">
+            <div class="cardNewsImg"></div>
+            <h5 class="tagMaisNews">EVENTOS</h5>
+            <h3>Teste de título</h3>
+            <h4>Aqui vai o subtítulo da matéria, exclusiva ou press release</h4>
+        </div>
+        <div class="cardNews">
+            <div class="cardNewsImg"></div>
+            <h5 class="tagMaisNews">EVENTOS</h5>
+            <h3>Teste de título</h3>
+            <h4>Aqui vai o subtítulo da matéria, exclusiva ou press release</h4>
+        </div>
+        <div class="cardNews">
+            <div class="cardNewsImg"></div>
+            <h5 class="tagMaisNews">EVENTOS</h5>
+            <h3>Teste de título</h3>
+            <h4>Aqui vai o subtítulo da matéria, exclusiva ou press release</h4>
+        </div>
+    </div>
+</section>
+
+<!-- BANNER -->
+
+<section class="banner01">
+    <div class="efeitoBanner01">PUBLICIDADE</div>
+</section>
+
+<!-- VAGAS -->
+
+<section id="vagas" class="container-fluid">
+    <div class="container">
+        <h1 class="Titulo titVagas">Procurando um trampo?</h1>
+    </div>
+    <div class="vagas container">
+        <div class="vaga">01</div>
+        <div class="vaga">02</div>
+        <div class="vaga">03</div>
+        <div class="vaga">04</div>
+        <div class="vaga">05</div>
+        <div class="vaga">06</div>
+        <div class="vaga">07</div>
+        <div class="vaga">08</div>
+    </div>
+</section>
+
+
 
 <?php include "footer.php" ?>
